e09fa3ad0646515ff89fb1247d9b5f247070acf1
taost <taost@localhost.localdomain>
2017-12-06 23:34:10 -0600

    adding swapping in endian

--- endian/main.c
+++ endian/main.c
@@ -10,16 +10,33 @@ int main()
 
 	char* addr = ( char* )&num;
 
-	printf ("&num is %i\n", &num);
-	printf ("addr is %i\n", addr);
+	printf ("&num is %p\n", &num);
+	printf ("addr is %p\n", addr);
 
 	int newNum = (int)(*addr);
 
+	// check if this CPU is big endian or small endian
 	if (newNum == 0x12) 
 		printf ("This cpu is big endian \n");
 	else if (newNum == 0x78) 
 		printf ("This cpu is small endian \n");
 	else 
 		printf ("something is wrong \n");
+
+	// Now swap it, convert 0x12345678 to 0x78563412
+
+	char* p1= (char*)(&num + 1);
+	char* p2= (char*)&newNum;
+	p1--;
+	for (int i=0; i<sizeof(int); i++)
+	{
+		printf("*p1 is %x\n", *p1);
+		printf("*p2 is %x\n", *p2);
+		*p2 = *p1;
+		p1--;
+		p2++;
+	}
+
+	printf("new number is %x\n", newNum);
 
 }
